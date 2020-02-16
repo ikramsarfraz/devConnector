d39616651e41dea47d3e66b38fe096cea216838d
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-02-16 05:57:08 -0500

    fixed documentation & create posts route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,7 +10,7 @@ const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 
 // @route  GET api/auth
-// @desc   Test route
+// @desc   Get user by token
 // @access Private
 router.get("/", auth, async (req, res) => {
   try {
