7461873c844a445b6685f08da9d9818cafd4fb81
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-02-25 17:28:53 -0500

    Display Profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -31,6 +31,7 @@ export const getCurrentProfile = () => async dispatch => {
 // Get All profiles
 export const getProfiles = () => async dispatch => {
   dispatch({ type: CLEAR_PROFILE });
+
   try {
     const res = await axios.get("/api/profile");
 
